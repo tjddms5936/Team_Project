77d0fb817bd3a2a3e9a665c8ed05bf7f8bf746fc
ParkSungEun <tjddms5936@naver.com>
2022-10-12 03:35:40 +0900

    commit

---
+++ Source/Term_Project/Term_Project.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Term_Project.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Term_Project, "Term_Project" );
---
+++ Source/Term_Project/Term_ProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Term_ProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TERM_PROJECT_API ATerm_ProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
